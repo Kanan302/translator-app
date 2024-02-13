e7d214d892c1d753b778cb3ae299cc3274b917aa
Kanan302 <kananalizade4@gmail.com>
2024-02-13 17:04:07 +0400

    added text to speech

--- lib/pages/views/translatormic.dart
+++ lib/pages/views/translatormic.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
+import 'dart:async';
 
 class TranslatorMic extends StatefulWidget {
   const TranslatorMic({super.key});
@@ -8,10 +10,36 @@ class TranslatorMic extends StatefulWidget {
 }
 
 class _TranslatorMicState extends State<TranslatorMic> {
+  final flutterTTS = FlutterTts();
+  final textController = TextEditingController();
+
+  Future<void> speak(String text) async {
+    await flutterTTS.setLanguage("en-US");
+    await flutterTTS.setVolume(0.8);
+    await flutterTTS.setSpeechRate(0.5);
+    await flutterTTS.setPitch(1);
+    await flutterTTS.speak(text);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Text('microphone')),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                children: [
+                  TextFormField(
+                    controller: textController,
+                  ),
+                  ElevatedButton(
+                      onPressed: () => speak(textController.text),
+                      child: const Text("Text to speak"))
+                ],
+              )),
+        ),
+      ),
     );
   }
 }
